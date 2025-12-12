a77103d11d52c6d6313bbd4176bdc629661b2faa
yugant-singh <yugantsingh9651@gmail.com>
2025-12-12 13:50:17 +0530

    Solve The practice Question Reagrding to the Promise chaining

--- script.js
+++ script.js
@@ -139,3 +139,200 @@
 // .then((data)=>{
 // console.log(data)
 // })
+
+// __________________________________________________
+
+
+// // ✅ Q7.
+
+// // Promise chain banao:
+
+// // First: return number 10
+
+// // Second: number * 2
+
+// // Third: number + 5
+// // Final output: 25
+
+// let pr  = new Promise((resolve , reject)=>{
+//     resolve(10);
+// })
+
+// pr
+// .then((first)=>{
+//     console.log(first)
+//     return first;
+// })
+// .then((second)=>{
+//     console.log(second)
+//     return second*2;
+// })
+// .then((third)=>{
+//     console.log(third)
+//     return third+5;
+
+// })
+// .then((final)=>{
+//     console.log(final);
+// })
+
+//__________________________________________________
+
+// ✅ Q8.
+
+// Promise ke andar error throw karke dekhna.
+// .catch() chalega ya nahi?
+// __________________________________________________
+
+
+
+
+// let  p = new Promise((resolve,rejected)=>{
+//     let value  = false;
+//     if(value){
+//         resolve(10);
+//     } else{
+//         rejected("something error");
+//     }
+// })
+
+// p
+// .then((value)=>{
+//     console.log("Step 1 :" , value);
+//     return value*2;
+// })
+// .then((value)=>{
+//     console.log("Step 2:", value);
+//     return value+5;
+// })
+// .then((value)=>{
+//     console.log("Final Step : ",value);
+// })
+// .catch((error)=>{
+//     console.log(error);
+// })
+
+
+
+
+// Ek promise banao jo 1 sec baad "A" return kare.
+// Chain:
+
+// A → A + "B"
+
+// AB → AB + "C"
+
+// ABC → print
+
+// Output: ABC
+
+
+// let p = new Promise((resolve,reject)=>{
+//     setTimeout(() => {
+//         resolve("A");
+//     }, 2000);
+// })
+
+// p
+// .then((value)=>{
+//     console.log("Step1 : ", value);
+//     return value+"B";
+// })
+// .then((value)=>{
+
+//     console.log("Step2:", value);
+//     return value+"C";
+    
+// })
+// .then((value)=>{
+//     console.log("Final Value:", value);
+// })
+// __________________________________________________
+
+// // ✅ Q4 (Medium)
+
+// // Ek promise banao jo number 10 return kare.
+// // Chain:
+
+// // Pehle number ka square
+
+// // Fir square ka double
+
+// // Fir 10 subtract
+// // Final value print karo.
+
+// let pr  = new Promise((resolve,reject)=>{
+//     resolve(10);
+// })
+
+// pr
+// .then((value)=>{
+//     console.log("Step1:", value);
+//     return value*value;
+// })
+// .then((value)=>{
+//     console.log("Square Phase:",value);
+//     return value*2;
+// })
+// .then((value)=>{
+//     console.log("Doubble Phase",value )
+//     return value-10;
+// })
+// .then((value)=>{
+//     console.log("Subtract Phase:", value);
+//     return value;
+// })
+// .then((value)=>{
+//     console.log("Final Value:", value);
+// })
+//___________________________________________________
+// // ✅ Q5 (Hard)
+
+// // Ek function getNumber(n) banao jo promise return karta ho:
+
+// // 1 sec baad number return kare
+// // Chain:
+// // getNumber(5)
+// // value * 4
+// // value - 3
+// // value / 2
+// // final result print
+
+
+
+// function getNumber(n){
+//     return new Promise((resolve,reject)=>{
+
+//         setTimeout(() => {
+            
+//             resolve(n);
+//         }, 1000);
+//     })
+// }
+// getNumber(10)
+// .then((value)=>{
+//     console.log("PAssed Value",value);
+//     return value*4;
+// })
+
+// .then((value)=>{
+//     console.log("Multiply Step:", value);
+//     return value-3;
+// })
+// .then((value)=>{
+//     console.log("Subract Step",value);
+//     return value/2;
+// })
+// .then((value)=>{
+//     console.log("Divide Step:", value);
+//     return value;    
+// })
+// .then((value)=>{
+//     console.log("Final Result :", value);
+    
+// })
+
+//__________________________________________________
+
+
+
